a36c747adf4e7de594c7d1d4dd74d2f222ceaf19
hsn <systemry420gmail.com>
2020-11-19 17:45:26 +0200

    add pages and implement quizes

--- app.js
+++ app.js
@@ -1,22 +1,51 @@
+// TODO: - load quizes for few seconds, and use kayrawan logo as loader
+
 const main = document.querySelector('main')
 window.addEventListener('load', init)
 
 function init() {
-    showLevels()
+    // showLevels()
 }
 
 function showLevels() {
     let html = ''
-    db.collection('level').get().then((snap)=>{
-        snap.docs.forEach(doc => {
-            let lev = doc.id
-            console.log(lev);
-            html += `
-                <section>
-                    <h1>${lev}</h1>
-                </section>
-            `
-            main.innerHTML = html
+
+    const ref = db.collection('level')
+    ref
+        .get()
+        .then(snap=>{
+            snap.docs.forEach(doc => {
+                let sec = doc.id
+                html += `
+                    <section onclick="showSpecs()">
+                        <h1>${sec}</h1>
+                    </section>
+                `
+                main.innerHTML = html
+            });
+
+        })    
+}
+
+function showSpecs() {
+    const docRef = db.collection('level').doc('ts')
+    console.log(docRef.id);
+    docRef.get().then((snap)=>{
+        console.log(snap);
+        // snap.forEach(doc => {
+        //     console.log(doc.data());
+        // });
+    })
+}
+
+function showShit() {
+    let html = ''
+        const docRef = db.collection('level').doc('bt').collection('accounting')
+        console.log(docRef);
+
+    docRef.get().then((snap)=>{
+        snap.forEach(doc => {
+            console.log(doc.id);
         });
     })
 }
